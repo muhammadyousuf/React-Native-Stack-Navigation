eda2776ff0829ffb290fb1c2bb865b72f4cccdb4
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-07-23 11:12:49 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Fragment } from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <Fragment>
+      <View>
+        <Text>Muhammad Yousuf</Text>
+      </View>
+    </Fragment>
+  );
+};
+
+
+
+export default App;
